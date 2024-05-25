dd6e92cbe6574e4edd1e86e65a1a4ed92c56409d
gkumar-pandey <gautamshekhar078@gmail.com>
2024-05-26 02:27:37 +0530

    feat: redirect to imdb page on click on movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,9 +7,10 @@ import {
   removeMovieFromFavorite,
 } from "../store/slice/movieSlice";
 import { checkMovieIsFav } from "../utils";
+import { Link } from "react-router-dom";
 
 const MovieCard = (props) => {
-  const { id, image, imgdb_url, movie, rating } = props;
+  const { id, image, imdb_url, movie, rating } = props;
   const { favMovies } = useSelector((state) => state.movies);
   const dispatch = useDispatch();
   const isFavorite = checkMovieIsFav(id, favMovies);
@@ -18,23 +19,24 @@ const MovieCard = (props) => {
       ? dispatch(removeMovieFromFavorite(id))
       : dispatch(addMovieToFavorite(props));
   };
-  return (
-    <>
-      <div className="  max-w-[400px]  border relative overflow-hidden h-full flex flex-col gap-4 border-[#3f3f4666]  p-6 rounded-md bg-[var(secondary-bg)] text-white w-full bg-base-100 shadow-xl">
-        <img src={image} alt={movie} />
 
+  return (
+    <div className="  max-w-[400px]  border relative overflow-hidden h-full flex flex-col gap-4 border-[#3f3f4666]  p-6 rounded-md bg-[var(secondary-bg)] text-white w-full bg-base-100 shadow-xl">
+      <Link to={imdb_url} target="_blank">
+        <img src={image} alt={movie} />
         <div className="card-body">
           <h2 className="text-lg font-medium text-white">{movie}</h2>
           <p>Rating : {rating}</p>
         </div>
-        <button
-          onClick={toggleFavorite}
-          className="text-3xl  top-2 right-2 cursor-pointer absolute"
-        >
-          {isFavorite ? <MdFavorite /> : <MdFavoriteBorder />}
-        </button>
-      </div>
-    </>
+      </Link>
+      <button
+        onClick={toggleFavorite}
+        type="button"
+        className="text-3xl  top-2 right-2 cursor-pointer absolute"
+      >
+        {isFavorite ? <MdFavorite /> : <MdFavoriteBorder />}
+      </button>
+    </div>
   );
 };
 
